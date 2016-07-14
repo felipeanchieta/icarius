4131b9acd09d0ed8023d1688757b6283f6c0946b
Felipe Anchieta <felipeanchieta@hotmail.com.br>
2016-07-14 01:31:03 -0300

    Added layout and scripts

---
+++ src/main/resources/static/js/icarius-js.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    if ($(location).attr('pathname').localeCompare('') != 0) {
+        var $currPage = $(location).attr('pathname').split('/')[1] + '-nav';
+        $('.header-nav li').each(function (i) {
+            0 == $(this).attr("id").localeCompare($currPage) ? $(this).addClass("active") : $(this).removeClass("active");
+        });
+    }
+});
