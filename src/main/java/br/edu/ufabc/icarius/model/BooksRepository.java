8d5678b23cf5aaccd80662738b348d9539a65836
Felipe Anchieta <felipeanchieta@hotmail.com.br>
2016-07-17 01:54:23 -0300

    Added JPA files

--- src/main/java/br/edu/ufabc/icarius/model/BooksRepository.java
+++ src/main/java/br/edu/ufabc/icarius/model/BooksRepository.java
@@ -4,10 +4,16 @@ import br.edu.ufabc.icarius.model.entities.Book;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by fcosta on 15/07/16.
  */
 public interface BooksRepository extends CrudRepository<Book, Long> {
+    Set<Book> findByIsbn(String isbn);
     List<Book> findByTitle(String title);
+    Set<Book> findByAuthorList(String... authorList);
+    Set<Book> findByPress(String press);
+    Set<Book> findByLocation(String location);
+    Set<Book> findByYear(int year);
 }
--- src/main/java/br/edu/ufabc/icarius/model/entities/User.java
+++ src/main/java/br/edu/ufabc/icarius/model/entities/User.java
@@ -1,7 +1,95 @@
 package br.edu.ufabc.icarius.model.entities;
 
+import javax.persistence.*;
+import java.util.Calendar;
+import java.util.List;
+
 /**
  * Created by fcosta on 15/07/16.
  */
+@Entity
 public class User {
+    @Id
+    private Long cpf;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    @Temporal(TemporalType.DATE)
+    private Calendar birthDate;
+
+    @OneToMany
+    private List<Book> loanBooks;
+
+    @OneToMany
+    private List<Book> reserves;
+
+    public User() {
+    }
+
+    public User(Long cpf, String name, String username, String password) {
+        this.cpf = cpf;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Long cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Calendar getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Calendar birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public List<Book> getLoanBooks() {
+        return loanBooks;
+    }
+
+    public void setLoanBooks(List<Book> loanBooks) {
+        this.loanBooks = loanBooks;
+    }
+
+    public List<Book> getReserves() {
+        return reserves;
+    }
+
+    public void setReserves(List<Book> reserves) {
+        this.reserves = reserves;
+    }
 }
